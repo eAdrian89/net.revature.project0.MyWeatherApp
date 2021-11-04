982110d490c8e1b8008b50f1a41528985ded369b
Adrian <e.adrianwojcik@gmail.com>
2021-11-04 21:20:21 +0100

    Logger implementation

---
+++ src/main/java/backend/Controllers/Configuration.java
@@ -0,0 +1,7 @@
+package backend.Controllers;
+
+public interface Configuration {
+    String URL = "jdbc:mysql://localhost:3306/jdbc_schema?serverTimezone=Europe/Warsaw";
+    String USER = "root";
+    String PASSWORD = "Master89";
+}
